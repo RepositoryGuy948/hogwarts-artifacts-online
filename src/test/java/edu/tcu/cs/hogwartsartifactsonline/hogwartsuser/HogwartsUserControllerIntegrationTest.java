934115f434a8cc3bb14cd1be6771ce30d950b2fb
Ali Farhat <alifarhat@Alis-MacBook-Pro-2.local>
2026-03-31 19:48:36 -0500

    Fixes

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/HogwartsUserControllerIntegrationTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/HogwartsUserControllerIntegrationTest.java
@@ -13,11 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -53,8 +54,13 @@ class HogwartsUserControllerIntegrationTest {
     String baseUrl;
 
     @Container
-    @ServiceConnection
     static RedisContainer redisContainer = new RedisContainer(DockerImageName.parse("redis:6.2.6"));
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", redisContainer::getHost);
+        registry.add("spring.redis.port", redisContainer::getFirstMappedPort);
+    }
 
 
     @BeforeEach
